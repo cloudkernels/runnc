2617b6139d0aba59c2181fb269aa682481f63e46
Charalampos Mainas <charalampos.mainas@gmail.com>
2019-10-22 12:28:21 +0300

    Find all isos in a directory and store their full path
    Signed-off-by: Charalampos Mainas <charalampos.mainas@gmail.com>

--- llmodules/fs/iso_storage.go
+++ llmodules/fs/iso_storage.go
@@ -3,7 +3,9 @@ package fs
 import (
 	"os"
 	"path/filepath"
+	"io/ioutil"
 
+	"strings"
 	"github.com/nabla-containers/runnc/libcontainer/configs"
 	ll "github.com/nabla-containers/runnc/llif"
 	"github.com/nabla-containers/runnc/nabla-lib/storage"
@@ -19,6 +21,7 @@ func NewISOFsHandler() (ll.FsHandler, error) {
 
 func (h *iSOFsHandler) FsCreateFunc(i *ll.FsCreateInput) (*ll.LLState, error) {
 	fsPath, err := createRootfsISO(i.Config, i.ContainerRoot)
+	isoPaths, err := findAllISOs(i.Config)
 	if err != nil {
 		return nil, errors.Wrap(err, "Unable to create rootfs ISO")
 	}
@@ -26,6 +29,7 @@ func (h *iSOFsHandler) FsCreateFunc(i *ll.FsCreateInput) (*ll.LLState, error) {
 	ret := &ll.LLState{}
 	ret.Options = map[string]string{
 		"FsPath": fsPath,
+		"FsStoragePath": isoPaths,
 	}
 
 	return ret, nil
@@ -42,7 +46,29 @@ func (h *iSOFsHandler) FsDestroyFunc(i *ll.FsDestroyInput) (*ll.LLState, error) 
 	return i.FsState, nil
 }
 
+func findAllISOs(config *configs.Config) (string, error) {
+	isoPath := config.IsoPaths
+	var isos []string
+	files, err := ioutil.ReadDir(isoPath)
+	if err != nil {
+		return "",err
+	}
+	for _, f := range files {
+		isoFilePath := filepath.Join(isoPath, f.Name())
+		if err != nil {
+			return "",err
+		}
+		isos = append(isos, isoFilePath)
+	}
+	return strings.Join(isos, ","),nil
+}
+
 func createRootfsISO(config *configs.Config, containerRoot string) (string, error) {
+	// edw ftiaxnetai to rootfs.iso, apo ta periexomena tou rootfs directory pou
+	// exei diabasei apo to config
+	// TODO: na doume pws tha erxetai apo to docker to directory me ta isos 
+	// gia thn wra tha ftiaksw ena direcotry opou tha periexei kapoia isos kai 
+	// tha xrhsimopoiw auta
 	rootfsPath := config.Rootfs
 	targetISOPath := filepath.Join(containerRoot, "rootfs.iso")
 	if err := os.MkdirAll(filepath.Join(rootfsPath, "/etc"), 0755); err != nil {
